ff78be9b340d70bc75d2c411011f472949c958bb
priyamkarn <priyamkarn77@gmail.com>
2024-10-17 06:22:38 +0530

    nodemailer

---
+++ index.js
@@ -0,0 +1,27 @@
+var nodejsmailer = require('nodemailer');
+
+var mailOptions = {
+    from: 'priyamkarn77@gmail.com',  // Correct email format
+    to: 'priyamkarn54@gmail.com',
+    subject: "Sending Email to priyam",
+    text: "Welcome to NodeMailer, It's Working",
+    html: '<h1>Welcome</h1><p>That was easy!</p>'
+};
+
+var transporter = nodejsmailer.createTransport({
+    service: 'gmail',
+    auth: {
+        user: 'priyamkarn77@gmail.com',  // Correct email format
+        pass: 'your app passcode' 
+
+    }
+});
+
+// Send the mail
+transporter.sendMail(mailOptions, function(error, info) {
+    if (error) {
+        console.log(error);
+    } else {
+        console.log('Email sent: ' + info.response);
+    }
+});
